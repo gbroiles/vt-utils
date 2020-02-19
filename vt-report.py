048cba4ab3ffc56d79904d8b62b7f42eb274b9ec
gbroiles <gbroiles@gmail.com>
2020-02-19 13:25:06 -0800

    reformat w/ black module

--- vt-report.py
+++ vt-report.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-#pylint: disable=invalid-name,missing-module-docstring,missing-function-docstring
+# pylint: disable=invalid-name,missing-module-docstring,missing-function-docstring
 import argparse
 import hashlib
 import os
@@ -7,21 +7,22 @@ import sys
 import pprint
 import requests
 
+
 def create_parse():
-    parser = argparse.ArgumentParser(
-        description='virustotal file report retriever')
-    parser.add_argument('filename', help='file to check for results')
+    parser = argparse.ArgumentParser(description="virustotal file report retriever")
+    parser.add_argument("filename", help="file to check for results")
     return parser
 
 
 def scanit(filename, apikey):
-    url = 'https://www.virustotal.com/vtapi/v2/file/report'
+    url = "https://www.virustotal.com/vtapi/v2/file/report"
     f = open(filename, "rb")
     contents = f.read()
     sha256 = hashlib.sha256(contents).hexdigest()
-    params = {'apikey': apikey, 'resource': sha256, 'allinfo': True}
+    params = {"apikey": apikey, "resource": sha256, "allinfo": True}
     response = requests.get(url, params=params)
     pprint.pprint(response.json())
+
 
 def start():
     parser = create_parse()
@@ -29,9 +30,10 @@ def start():
     try:
         apikey = os.environ["VTAPI"]
     except KeyError:
-        print('Must set VTAPI key enviroment variable.')
+        print("Must set VTAPI key enviroment variable.")
         sys.exit(1)
     scanit(args.filename, apikey)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     start()
--- vt-upload.py
+++ vt-upload.py
@@ -1,23 +1,25 @@
 #! /usr/bin/env python3
-#pylint: disable=invalid-name,missing-module-docstring,missing-function-docstring
+# pylint: disable=invalid-name,missing-module-docstring,missing-function-docstring
 import argparse
 import os
 import sys
 import pprint
 import requests
 
+
 def create_parse():
-    parser = argparse.ArgumentParser(
-        description='virustotal file uploader')
-    parser.add_argument('filename', help='file to scan')
+    parser = argparse.ArgumentParser(description="virustotal file uploader")
+    parser.add_argument("filename", help="file to scan")
     return parser
 
+
 def scanit(filename, apikey):
-    url = 'https://www.virustotal.com/vtapi/v2/file/scan'
-    params = {'apikey': apikey}
-    files = {'file': (filename, open(filename, 'rb'))}
+    url = "https://www.virustotal.com/vtapi/v2/file/scan"
+    params = {"apikey": apikey}
+    files = {"file": (filename, open(filename, "rb"))}
     response = requests.post(url, files=files, params=params)
     pprint.pprint(response.json())
+
 
 def start():
     parser = create_parse()
@@ -25,9 +27,10 @@ def start():
     try:
         apikey = os.environ["VTAPI"]
     except KeyError:
-        print('Must set VTAPI key enviroment variable.')
+        print("Must set VTAPI key enviroment variable.")
         sys.exit(1)
     scanit(args.filename, apikey)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     start()
