e13fdfd2a324dcdaca5abf9ca5a3e07f0e7d9f40
gbroiles <gbroiles@gmail.com>
2020-03-02 19:22:38 -0800

    add addl hash recognition for report fetching

--- virustotal.py
+++ virustotal.py
@@ -9,7 +9,18 @@ HEXCHARS = "0123456789abcdef"
 def scan(filename, apikey):
     """ checks virustotal for given filename hash for pre-generated reports """
     url = "https://www.virustotal.com/vtapi/v2/file/report"
-    if len(filename) == 64 and all(x in filename for x in HEXCHARS):
+    count = len(filename)
+    if (count == 64 or count == 40 or count == 32) and all(
+        x in filename for x in HEXCHARS
+    ):
+        print("Treating {} as a hash, not as a filename".format(filename))
+        sha256 = filename
+    elif (
+        count == 75
+        and filename[64] == "-"
+        and all(x in filename for x in HEXCHARS + "-")
+    ):
+        print("Treating {} as a VirusTotal scan ID, not as a filename".format(filename))
         sha256 = filename
     else:
         with open(filename, "rb") as infile:
